e287f7086b86603a166662fc97071c35fdfac1ef
kennethhagist <kenhagist@gmail.com>
2017-12-19 12:14:34 -0800

    Adding Github Score assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { GithubService } from './github.service';
 
 
 @NgModule({
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [
+    GithubService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
